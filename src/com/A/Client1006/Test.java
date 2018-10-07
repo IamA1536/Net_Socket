2c6d34b3fa600f5a306d0f015dafa1c11ad04de0
IamA1536 <18974735866@163.com>
2018-10-07 09:48:17 +0800

    Complete private chat

--- src/com/A/Client1006/Test.java
+++ src/com/A/Client1006/Test.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("Note: message@username(who you want to send message, default is all users), if you want to send it to all users, please @All");
         System.out.println("Please input user's name:");
         Scanner input = new Scanner(System.in);
         Client client = new Client(input.nextLine());
--- src/com/A/Server1006/ClientMsg.java
+++ src/com/A/Server1006/ClientMsg.java
@@ -25,22 +25,40 @@ public class ClientMsg implements Runnable, Data {
         try {
             BufferedInputStream BIS = new BufferedInputStream(socket.getInputStream());
             byte[] b = new byte[serverbyte];
+            Socket fromuser = null;
             String msg;
+            String[] msgbody = new String[2];
             while (true) {
                 int c = 0;
                 String user = "User";
                 int i = BIS.read(b);
                 if (i == -1) break;
                 msg = new String(b, 0, i);
+                if (msg.indexOf("@") == -1) {
+                    msgbody[0] = msg;
+                    msgbody[1] = "All";
+                } else msgbody = msg.split("@", 2);
                 for (Socket s : clientlist) {
                     c++;
                     if (s.getPort() == socket.getPort()) {
                         user = userName.get(c - 1);
+                        fromuser = s;
                         break;
                     }
                 }
-                for (Socket s : clientlist) sendMsg(s, user + ":" + msg);
-
+                if (msgbody[1].equals("All"))
+                    for (Socket s : clientlist) sendMsg(s, user + ":" + msgbody[0]);
+                else {
+                    sendMsg(fromuser, user + ":" + msgbody[0]);
+                    c = 0;
+                    for (Socket s : clientlist) {
+                        c++;
+                        if (msgbody[1].equals(userName.get(c - 1))) {
+                            sendMsg(s, user + ":" + msgbody[0]);
+                            break;
+                        }
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -48,6 +66,7 @@ public class ClientMsg implements Runnable, Data {
         }
 
     }
+
 
     private void sendMsg(Socket fromSocket, String msg) {
         try {
